70bf5023765deeaaeb3150bb664944158bec572d
Evgeny Sitchikhin <sitchikhin@gmail.com>
2026-02-04 18:56:47 +0300

    feat(app): Разработка сервиса #6

--- internal/ws/server/server.go
+++ internal/ws/server/server.go
@@ -29,9 +29,24 @@ func New(addr string, wsHandler, standsHandler http.Handler) *Server {
 	return &Server{
 		httpServer: &http.Server{
 			Addr:    addr,
-			Handler: mux,
+			Handler: corsMiddleware(mux),
 		},
 	}
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 // Run запускает сервер и настраивает graceful shutdown.
